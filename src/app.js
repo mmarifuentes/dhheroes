38879849c3c95b17c526a2dad7a4b3ea807f0eba
mmarifuentes <maria.fuentes1203@gmail.com>
2023-07-26 01:46:24 -0300

    agregando proyecto node

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");   
+
+const app = express();  
+
+const path = require("path");
+
+app.listen(3030, () => console.log("servidor corriendo en el puerto 3030"));
+
+
+
